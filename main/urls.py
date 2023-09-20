16c969ea4fef00a3c965ee807ff8a9f98b426b17
Joseph BIntang <joseph.bintang@ui.ac.id>
2023-09-20 13:46:32 +0700

    push tutorial 3

--- main/urls.py
+++ main/urls.py
@@ -3,11 +3,17 @@ from main.views import show_main
 from main.views import show_main, create_product
 from main.views import show_main, create_product, show_xml 
 from main.views import show_main, create_product, show_xml, show_json
+from main.views import register
+from main.views import login_user
+from main.views import logout_user
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
     path('create-product', create_product, name='create_product'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
