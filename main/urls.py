236a7b097195183bc5c6e834b0992e8921db2f41
Joseph BIntang <joseph.bintang@ui.ac.id>
2023-09-27 14:01:00 +0700

    push tutorial 4

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from main.views import show_main
 from main.views import show_main, create_product
-from main.views import show_main, create_product, show_xml 
+from main.views import show_main, create_product, show_xml
 from main.views import show_main, create_product, show_xml, show_json
 from main.views import register
 from main.views import login_user
 from main.views import logout_user
+from main.views import edit_product
+from main.views import delete_product
 
 app_name = 'main'
 
@@ -17,4 +19,6 @@ urlpatterns = [
     path('create-product', create_product, name='create_product'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
+    path('edit-product/<int:id>', edit_product, name='edit_product'),
+    path('delete/<int:id>', delete_product, name='delete_product'),
 ]
